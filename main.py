b4104e5285b88a9fc7da21efeedefe5334cfa5a7
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-09-04 09:02:44 +1000

    Set up .venv and image

---
+++ main.py
@@ -0,0 +1,10 @@
+# TODO ASCII ART GENERATOR
+# First: Turn images into ASCII ART
+# Second: Randomly generate faces.
+# Third: Create town?
+
+from PIL import Image
+
+img = Image.open("test_img.jpg")
+
+img.show()
