1706e55b95e9b039229d67c191679a70b564a263
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-09-07 14:48:31 +1000

    Random phrase generator

--- main.py
+++ main.py
@@ -21,9 +21,11 @@
 #       - Settings page to manage word limit, GUI colours, and email notifications
 
 from PySide6.QtWidgets import (QApplication, QLabel, QLayout, QGroupBox, QTextEdit, 
-                               QMainWindow, QStackedWidget, QPushButton, QWidget, QVBoxLayout)
+                               QMainWindow, QStackedWidget, QPushButton, QWidget, 
+                               QVBoxLayout, QHBoxLayout)
 from PySide6.QtCore import QCalendar, Qt
 from PySide6.QtGui import QFont
+from random import choice
 
 
 class MainWindow(QMainWindow):
@@ -32,35 +34,59 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Lightbulb Factory")
         self.showFullScreen()
+        title_font = QFont()
 
         # HOME PAGE
+        # TODO: Update GUI, save idea to a SQL database
         self.home_page = QWidget()
-        title_font = QFont()
         title_font.setPointSize(40)
 
         home_page_layout = QVBoxLayout(self.home_page)
         home_title = QLabel("Welcome to the factory")
         home_title.setFont(title_font)
-        home_title.setAlignment(Qt.AlignCenter)
 
         idea_text_box = QTextEdit(placeholderText="Write your idea")
+        idea_text_box.setFixedSize(750, 300)
 
         submit_button = QPushButton("Save")
-        submit_button.clicked.connect(lambda: print(idea_text_box.toPlainText()))
+        submit_button.clicked.connect(lambda: self.clear_idea(idea_text_box))
 
-
-        home_page_layout.addWidget(home_title)
-        home_page_layout.addWidget(idea_text_box)
+        home_page_layout.addWidget(home_title, alignment=Qt.AlignCenter)
+        home_page_layout.addWidget(idea_text_box, alignment=Qt.AlignCenter)
         home_page_layout.addWidget(submit_button)
 
 
-        # Prompt Page
+        # PROMPT
         self.prompt_page = QWidget()
         prompt_page_layout = QVBoxLayout(self.prompt_page)
         prompt_page_title =  QLabel("Click for a prompt")
+        prompt_page_title.setFont(title_font)
         prompt_page_title.setAlignment(Qt.AlignCenter)
 
+        prompt_frame = QWidget()
+        prompt_frame_layout = QHBoxLayout(prompt_frame)
+        label1 = QLabel("ONE")
+        label2 = QLabel("TWO")
+        label3 = QLabel("THREE")
+
+        for label in [label1, label2, label3]:
+            label.setAlignment(Qt.AlignCenter)
+            prompt_frame_layout.addWidget(label)
+
+        spark_button = QPushButton("Generate")
+        spark_button.clicked.connect(lambda: self.generate_prompt(label1, label2, label3))
+
         prompt_page_layout.addWidget(prompt_page_title)
+        prompt_page_layout.addWidget(prompt_frame)
+        prompt_page_layout.addWidget(spark_button)
+
+
+        # VIEW PAGE
+
+        # COMPLETED PAGE
+
+        # STREAK PAGE
+
 
 
         # Create stacked widget
@@ -79,7 +105,8 @@ class MainWindow(QMainWindow):
         spark.triggered.connect(self.show_spark_page)
 
         view = menubar.addAction("View")
-        completed = menubar.addAction("Home")
+        completed = menubar.addAction("Completed")
+        streak = menubar.addAction("Streak")
 
         quit = menubar.addAction("Quit")
         quit.triggered.connect(lambda: self.close())
@@ -89,6 +116,33 @@ class MainWindow(QMainWindow):
 
     def show_spark_page(self):
         self.pages.setCurrentWidget(self.prompt_page)
+
+    # FUNCTIONS FOR HOME PAGE
+    def clear_idea(self, text_field):
+        print(text_field.toPlainText())
+        text_field.clear()
+
+    # FUNCTIONS FOR SPARK PAGE
+    # Gives a noun-verb-noun prompt when the generate button is pressed
+    def generate_prompt(self, label1, label2, label3):
+        with open("word_lists/nouns_5000.txt") as file:
+            noun_list = file.readlines()
+            first_noun = choice(noun_list)
+            second_noun = choice(noun_list)
+
+            # Checks if the second noun was already selected
+            while second_noun == first_noun:
+                second_noun = choice(noun_list)
+
+            label1.setText(first_noun)
+            label3.setText(second_noun)
+
+        with open("word_lists/verbs_5000.txt") as file:
+            verb = choice(file.readlines())
+
+            label2.setText(verb)
+
+              
     
 
         
