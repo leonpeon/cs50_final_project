a5eaf650f75d60af6aee24747fdea37bb0f5bac4
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-09-11 16:00:12 +1000

    Update word txt files

--- app_pages/home.py
+++ app_pages/home.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import (QComboBox, QLabel, QTextEdit,
                                QVBoxLayout)
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
+from random import choice
 
 
 # Handles the home page
@@ -21,6 +22,10 @@ class HomePage(QWidget):
         self.idea_text_box = QTextEdit(placeholderText="Write your idea")
         self.idea_text_box.setFixedSize(750, 300)
 
+        # Generate prompt button
+        self.prompt_button = QPushButton("Generate spark")
+        self.prompt_button.clicked.connect(self.generate_prompt)
+
         # Tags
         self.tags_combobox = QComboBox(placeholderText="Add tag", duplicatesEnabled=False)
         self.tags_combobox.addItems([
@@ -37,5 +42,19 @@ class HomePage(QWidget):
 
         layout.addWidget(self.home_title, alignment=Qt.AlignCenter)
         layout.addWidget(self.idea_text_box, alignment=Qt.AlignCenter)
+        layout.addWidget(self.prompt_button, alignment=Qt.AlignCenter)
         layout.addWidget(self.tags_combobox, alignment=Qt.AlignCenter)
         layout.addWidget(self.submit_button)
+
+
+    def generate_prompt(self):
+        with open("./word_lists/adjectives.txt") as file:
+            adjective = choice(file.readlines())
+
+        with open("./word_lists/nouns.txt") as file:
+            noun = choice(file.readlines())
+
+        with open("./word_lists/verbs.txt") as file:
+            verb = choice(file.readlines())
+
+        self.home_title.setText(f"{adjective} {noun} {verb}")
